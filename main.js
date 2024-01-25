d44f862561d875c3d2552369f001e6a21b437ec1
Sathish M <sathishms0305@gmail.com>
2024-01-25 12:06:08 +0530

    delete all and single item complete

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ console.log(itemsArray.length)
 let itemsPurchasedCount = (item)=>{
     let cart = 0
     item.forEach((i)=>{
-        let item_ob = JSON.parse(i)
+        let item_ob = i
         if(item_ob.item_status === 'purchased'){
             cart+=1
         }
@@ -36,16 +36,18 @@ let itemsCounter = (totalItemsCount,itemsPurchasedCount)=>{
     totalItems.innerHTML = `Total number of items Purchased ${itemsPurchasedCount} / ${totalItemsCount}`
 }
 
-itemsCounter(totalItemsCount(itemsArray),itemsPurchasedCount(itemsArray))
+let updateCounts = () => {
+    itemsCounter(totalItemsCount(itemsArray), itemsPurchasedCount(itemsArray));
+};
 
 let saveItem = (item)=>{
-    console.log(JSON.parse(item))
+    console.log(item)
     let card = document.createElement('div')
     card.classList.add('card')
     
     let itemDetail = document.createElement('div')
     itemDetail.classList.add('item-detail')
-    itemDetail.innerHTML = `${JSON.parse(item).item_name} ${JSON.parse(item).quantity}${JSON.parse(item).metrics}`
+    itemDetail.innerHTML = `${item.item_name} ${item.quantity}${item.metrics}`
     card.appendChild(itemDetail)
     
     let statusBox = document.createElement('div')
@@ -61,64 +63,90 @@ let saveItem = (item)=>{
     notPurchased.classList.add('not-purchased')
     statusBox.appendChild(notPurchased)
 
-    let edit = document.createElement('div')
-    edit.classList.add('edit')
-    statusBox.appendChild(edit)
-
     let deleteItem = document.createElement('div')
     deleteItem.classList.add('delete')
     statusBox.appendChild(deleteItem)
 
 
     purchased.addEventListener('click',()=>{
-        let itemObject = JSON.parse(item)
-        itemObject.item_status = 'purchased'
-        let str_itemobj = JSON.stringify(itemObject)
-        itemsArray[itemsArray.indexOf(item)] = str_itemobj
+        item.item_status = 'purchased';
+        updateItemInArray(item);
         localStorage.setItem('items',JSON.stringify(itemsArray))
         card.style.background = '#8ac926'
-        itemsCounter(totalItemsCount(itemsArray),itemsPurchasedCount(itemsArray))
-        location.reload()
+        updateCounts();
     })
 
     notPurchased.addEventListener('click',()=>{
-        let itemObject = JSON.parse(item)
-        itemObject.item_status = 'not purchased'
-        let str_itemobj = JSON.stringify(itemObject)
-        itemsArray[itemsArray.indexOf(item)] = str_itemobj
+        item.item_status = 'not purchased'
+        updateItemInArray(item);
         localStorage.setItem('items',JSON.stringify(itemsArray))
         card.style.background = '#FCC72C'
-        itemsCounter(totalItemsCount(itemsArray),itemsPurchasedCount(itemsArray))
+        updateCounts();
+    })
+    
+
+    deleteItem.addEventListener('click',()=>{
+        section.removeChild(card)
+        let res = itemsArray.indexOf(item)
+        if (res > -1) {
+            itemsArray.splice(res, 1);  // Remove the element
+        }
+        localStorage.setItem('items',JSON.stringify(itemsArray))
         location.reload()
     })
 
 
-    let task_object = JSON.parse(item)
+    let task_object = item
     if(task_object.item_status === 'not purchased'){
         card.style.background = '#FCC72C'
-        itemsCounter(totalItemsCount(itemsArray),itemsPurchasedCount(itemsArray))
+        updateCounts();
     }else{
         card.style.background = '#8ac926'
-        itemsCounter(totalItemsCount(itemsArray),itemsPurchasedCount(itemsArray))
+        updateCounts();
     }
 }
+
+let updateItemInArray = (updatedItem) => {
+    let index = itemsArray.findIndex(item => item.id === updatedItem.id);
+    if (index !== -1) {
+        itemsArray[index] = updatedItem;
+    }
+};
+
 
 itemsArray.forEach(saveItem)
 
 let addItem = () => {
     let item = {
+        id: Date.now(),
         item_name:itemName.value,
         quantity:quantity.value,
         metrics:metrics.value,
         item_status:'not purchased'
     }
-    itemsArray.push(JSON.stringify(item))
+    itemsArray.push(item)
     localStorage.setItem('items', JSON.stringify(itemsArray))
-    saveItem(JSON.stringify(item))
-    itemsCounter(totalItemsCount(itemsArray),itemsPurchasedCount(itemsArray))
+    saveItem(item)
+    updateCounts();
     itemName.value = ''
     quantity.value = ''
     metrics.value = ''
 }
 
+let deleteAll = ()=>{
+    if(itemsArray.length === 0){
+        alert('No Tasks to Delete')
+    }else{
+        localStorage.clear()
+        itemsArray.length = 0;
+        while (section.firstChild) {
+            section.removeChild(section.firstChild);
+        }
+        location.reload()
+    }
+}
+
+
+
+deleteAllBtn.addEventListener('click',deleteAll)
 addItemBtn.addEventListener('click', addItem)
